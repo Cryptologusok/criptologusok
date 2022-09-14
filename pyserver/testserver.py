fdc19b599cd9bae2db288925119878d0be2b0c8e
Németh Dániel <nemeth.daniel@sze.hu>
2022-09-14 20:42:16 +0200

    create default directory structure

--- pyserver/testserver.py
+++ pyserver/testserver.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     alma="This is test data passed to htmls"
-    return render_template("background/background.html", main="index.html", content=alma, words=["This ", "is ", "how ", "you ", "pass ", "an ", "array."])
+    return render_template("background/background.html", main="index/index.html", content=alma, words=["This ", "is ", "how ", "you ", "pass ", "an ", "array."])
      
 
 @app.route("/test")
 def test():
     alma="This is the test page, hy"
-    return render_template("background/background.html", main="index.html",content=alma, words=["This ", "is ", "how ", "you ", "pass ", "an ", "array."])
+    return render_template("background/background.html", main="index/index.html",content=alma, words=["This ", "is ", "how ", "you ", "pass ", "an ", "array."])
+
+@app.route("/other")
+def other():
+    return render_template("background/background.html", main="another/another.html")
 
 
 if __name__ == "__main__":
