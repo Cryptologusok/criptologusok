36b29b1251b7eeee557ebb4422846f0a42f411c8
Németh Dániel <nemeth.daniel@sze.hu>
2022-10-01 10:21:52 +0200

    implement de code

--- pyserver/services/de.py
+++ pyserver/services/de.py
@@ -1,17 +1,38 @@
-import random
+import duckdb
+import pandas as pd
+import datetime
 
-def getMinMax(startDate,endDate,xaxist):
-    min=50
-    max=150
-    return (min,max)
+con = duckdb.connect()
+pd_data = pd.read_parquet('input_single_json.parquet')
 
-def getYaxisData(startDate,endDate,xaxis,yaxis):
-    resoult = [[]]
-    for i in range(len(yaxis)):
-        resoult.append([])
-    for i in range(1000):
-        resoult[0] = random.randint(50,150)
-        for j in range(len(yaxis)):
-            resoult[j+1] = random.randint(0,100000)
-    return resoult
-    #[x,y1,y2,y3..y]
+# def getMaxMin(startDate,endDate,reqCol):
+#   #generate query
+#   queryStr =  "SELECT MIN(" + reqCol + ") , MAX(" + reqCol + ") "
+#   queryStr += " FROM pd_data where time > TIMESTAMP '" + startDate + "' and time < TIMESTAMP '" + endDate + "'"
+#   result = con.execute(queryStr).fetchone()
+#   return  ('{"minValue":' + str(result[0]) + ',"maxValue:"' + str(result[1]) + '}')
+
+def getData(startDate,endDate,xaxis,yaxis):
+  #splitting up yaxis for subquery
+  subQuerySelectColumns = xaxis
+  for y in yaxis:
+    subQuerySelectColumns = subQuerySelectColumns + ',' + y
+  #generating subquery
+  subQuery =  "SELECT " + subQuerySelectColumns
+  subQuery += " , NTILE(100) OVER (ORDER BY " + xaxis + " asc) bucket"
+  subQuery += " FROM pd_data where time > TIMESTAMP '" + startDate + "' and time < TIMESTAMP '" + endDate + "'"
+  #splitting up yaxis for mainquery
+  mainQuerySelectColumns = ' AVG(CAST(' + xaxis + ' AS BIGINT)) AS ' + xaxis 
+  for y in yaxis:
+    mainQuerySelectColumns = mainQuerySelectColumns + ',' + ' AVG(CAST(' + y + ' AS BIGINT)) AS ' + y
+  #generating mainquery
+  mainQuery = " SELECT bucket," + mainQuerySelectColumns
+  mainQuery += " FROM ( " + subQuery + " ) "
+  mainQuery += " GROUP BY bucket "
+  #execute query and return
+  #result = con.execute(mainQuery).fetchall()
+  result = con.execute(mainQuery).fetchall()
+  return (result)
+
+result = getData("2009-07-05 11:04:35","2022-07-05 11:04:35","cr_b",["bloc","it_s"])
+print(result)
