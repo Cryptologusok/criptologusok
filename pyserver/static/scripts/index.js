80fb0be56e323f94a8d2b34ee9f35e29439cad77
lefodor <levente.fodor@hotmail.com>
2022-10-15 06:49:54 +0200

    frontend: date filter functioning

--- pyserver/static/scripts/index.js
+++ pyserver/static/scripts/index.js
@@ -128,4 +128,26 @@ chartLine = new ApexCharts(document.querySelector("#chart-line"), optionsLine);
 chartLine.render();
 }
 
-getData();
+/* custom functions */
+const displayMessage = function (message) {
+  document.getElementById("data").innerHTML = message;
+};
+
+/* EventListeners */
+document.querySelector('.send').addEventListener('click', function () {
+
+  let _tmp_startDate = new Date(document.querySelector('.StartDate').value);
+  let _tmp_endDate = new Date(document.querySelector('.EndDate').value);
+
+  console.log(_tmp_startDate.getTime());
+  console.log(_tmp_endDate.getTime());
+
+  if(_tmp_startDate.getTime() > _tmp_endDate.getTime()) {
+    displayMessage('Start date must be earlier than end date...') ;
+  } else {
+    startDate = document.querySelector('.StartDate').value;
+    endDate = document.querySelector('.EndDate').value;
+    getData();
+  }
+
+});
